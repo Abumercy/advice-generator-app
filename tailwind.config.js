9afa39e73f301cad7e03ad7109967c442198b27d
Abumercy <101097127+Abumercy@users.noreply.github.com>
2022-10-29 21:02:26 +0100

    advice generator app

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        lightCyan: "hsl(193, 38%, 86%)",
+        neonGreen: "hsl(150, 100%, 66%)",
+        grayishBlue: "hsl(217, 19%, 38%)",
+        darkGrayishBlue: "hsl(217, 19%, 24%)",
+        DarkBlue: "hsl(218, 23%, 16%)",
+      },
+      fontFamily: {
+        body: ["Manrope"],
+      },
+    },
+  },
+  plugins: [],
+};
+
